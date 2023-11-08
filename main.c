ceb33b2a4715a53032e67d9a8d13735d7bb4e686
autrcuhe <150247179+autrcuhe@users.noreply.github.com>
2023-11-08 13:15:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int tableau[10][20];
+    printf("|*|*|*|*|*|*|*|*|*|*|*|*|\n");
+    for (int i = 1; i < 10; ++i) {// on mets 12 pour avoir 10 lignes vide plus deux lignes pleines qui correspondent au bors
+        printf("|*|");
+        for (int j = 1; j < 20; ++j) {
+            printf(" ");
+        }
+        printf("|*|\n");
+    }
+    printf("|*|*|*|*|*|*|*|*|*|*|*|*|");
     return 0;
-}jhbybu
-armand baise ta GROSSE MERE
-hgvhbjsnf
-lorainzzau
+}
